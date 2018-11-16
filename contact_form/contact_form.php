2eff1386f4cd2c7a55242ab58526b4b47dec7397
Darren Anastasoff <soleman16@gmail.com>
2018-11-16 15:45:36 -0500

    updated resume and portfolio page

--- contact_form/contact_form.php
+++ contact_form/contact_form.php
@@ -2,7 +2,7 @@
 
 // configure
 $from = 'Contact form <darren.anastasoff@gmail.com>';
-$sendTo = 'darren.anastasoff@gmail.com.com';
+$sendTo = 'darren.anastasoff@gmail.com';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
--- js/main.js
+++ js/main.js
@@ -272,7 +272,7 @@
         //Google Maps
         $("#map").googleMap();
         $("#map").addMarker({
-            address: "15 avenue des champs Elysées 75008 Paris" // Your Address
+            address: "Rollinsford, NH 03869" // Your Address
         });
     });
 

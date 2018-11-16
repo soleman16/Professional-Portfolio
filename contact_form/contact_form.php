f288421ba6f0039d4113e6adb9210d2ba3e9e5af
Darren Anastasoff <soleman16@gmail.com>
2018-11-16 12:31:03 -0500

    updated contact form

--- contact_form/contact_form.php
+++ contact_form/contact_form.php
@@ -1,8 +1,8 @@
 <?php
 
 // configure
-$from = 'Contact form <yourmail@domain.com>';
-$sendTo = 'your@mail.com';
+$from = 'Contact form <darren.anastasoff@gmail.com>';
+$sendTo = 'darren.anastasoff@gmail.com.com';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
